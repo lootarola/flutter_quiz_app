cc091a44610cda4c5a4702bd85cb9eedb765920f
Lucas Otarola Meneses <lotarolam@bancoripley.com>
2022-09-01 19:17:26 -0400

    feat: added answer buttons and print interaction to answer 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,36 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite color'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(children: [
+          Text('The question!'),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: null,
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: null,
+          ),
+        ]),
       ),
     );
   }
