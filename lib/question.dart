d8ad6589618e0ba3e814258863561db18d09ad54
Lucas Otarola Meneses <lotarolam@bancoripley.com>
2022-09-02 23:32:34 -0400

    feat: added question titles

--- lib/question.dart
+++ lib/question.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
+  final String questionTitle;
   final String questionText;
 
-  Question(this.questionText);
+  Question(this.questionTitle, this.questionText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       margin: EdgeInsets.all(10),
-      child: Text(
-        questionText,
-        style: TextStyle(fontSize: 28),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            questionTitle,
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.left,
+          ),
+          SizedBox(
+            height: 150,
+          ),
+          Text(
+            questionText,
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.left,
+          ),
+        ],
       ),
     );
   }
