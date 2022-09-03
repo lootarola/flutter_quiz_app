32925a6cf90ba0a1c378c0dfc567b22c6f6a37b6
Lucas Otarola Meneses <lotarolam@bancoripley.com>
2022-09-02 23:33:13 -0400

    feat: implementation of quiz and result widgets into main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,49 +13,83 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionTitle':
+          'Did you know that the first red card given on a football match ever was to a chilean player?',
+      'questionText': 'What was the name of this player?',
+      'answers': [
+        {'text': 'Elías Figueroa', 'score': 0},
+        {'text': 'Carlos Caszely', 'score': 1},
+        {'text': 'Patricio Yáñez', 'score': 0},
+        {'text': 'Iván Zamorano', 'score': 0},
+      ],
+    },
+    {
+      'questionTitle':
+          'Did you know that the first president of Chile wasn\'t chilean?',
+      'questionText': 'Where was the first president of Chile born?',
+      'answers': [
+        {'text': 'Perú', 'score': 0},
+        {'text': 'Bolivia', 'score': 0},
+        {'text': 'Uruguay', 'score': 0},
+        {'text': 'Argentina', 'score': 1}
+      ],
+    },
+    {
+      'questionTitle':
+          'Did you know that chilean Airport "Arturo Merino Benítez" is missing a boarding gate?',
+      'questionText': 'Which is the number of the gate missing?',
+      'answers': [
+        {'text': '8', 'score': 0},
+        {'text': '13', 'score': 1},
+        {'text': '21', 'score': 0},
+        {'text': '17', 'score': 0}
+      ],
+    },
+    {
+      'questionTitle':
+          'Did you know that Chile has the largest earthquake registered in the world?',
+      'questionText': 'What was its magnitude? (Richter Scale)',
+      'answers': [
+        {'text': '8,5', 'score': 0},
+        {'text': '9,0', 'score': 0},
+        {'text': '7,8', 'score': 0},
+        {'text': '9,5', 'score': 1}
+      ],
+    },
+  ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
     setState(() {
-      _questionIndex = _questionIndex + 1;
+      _questionIndex = 0;
+      _totalScore = 0;
     });
-    print(_questionIndex);
+  }
+
+  void _answerQuestion(int score) {
+    setState(() {
+      _questionIndex += 1;
+      _totalScore += score;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
-    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex],
-            ),
-            ElevatedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            ElevatedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                // ...
-                print('Answer 3 chosen');
-              },
-            ),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('QuizApp'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : Result(_totalScore, _resetQuiz)),
     );
   }
 }
